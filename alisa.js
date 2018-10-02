6d3e8fe299baa712d358a79d7e46fa13ed10ab30
sunday <parkh.dan@gmail.com>
2018-10-02 17:12:01 +0300

    cleanup

--- alisa.js
+++ alisa.js
@@ -3,9 +3,27 @@
 const Koa = require('koa');
 const app = new Koa();
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Alisa';
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Resonse-Time', `${ms}ms`);
 });
 
-app.listen(8888);
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method}: ${ctx.url} - ${ms}ms`)
+});
+
+// response
+app.use(async ctx => {
+  ctx.body = 'Hello you';
+});
+
+if (!module.parent) app.listen(8888);
